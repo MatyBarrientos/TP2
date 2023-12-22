7eeb5e8f602caa23feeda2ff4d994bf91c246a28
Matías Barrientos <r.m.barrientos@live.com>
2023-12-21 23:02:11 -0300

    proyecto bariloche en django

--- proyecto/experiencias/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ExperienciasConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "experiencias"
-    verbose_name = "experiencias"
